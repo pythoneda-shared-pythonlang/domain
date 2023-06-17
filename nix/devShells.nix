3a8c4b02911d436ab3ef3f33be1fa02efc873b4a
rydnr <github@acm-sl.org>
2023-06-17 18:54:19 +0200

    Added .pythoneda marker. 0.0.1a14

--- nix/devShells.nix
+++ nix/devShells.nix
@@ -9,11 +9,11 @@ rec {
     echo -e " \033[32m             _   _                          \033[35m_\033[0m";
     echo -e " \033[32m            | | | |                        \033[35m| | \033[37mGPLv3\033[0m";
     echo -e " \033[32m _ __  _   _| |_| |__   ___  _ __   \033[34m___  \033[35m__| | \033[36m__ _ \033[32mhttps://github.com/nixos/nixpkgs/$NIXPKGSRELEASE\033[0m";
-    echo -e " \033[32m| '_ \| | | | __| '_ \ / _ \| '_ \ \033[34m/ _ \\\\\033[35m/ _\` |\033[36m/ _\` |\033[34mhttps://github.com/pythoneda-application\033[0m";
-    echo -e " \033[32m| |_) | |_| | |_| | | | (_) | | | |\033[34m  __/\033[35m (_| |\033[36m (_| |\033[35mhttps://github.com/pythoneda-infrastructure\033[0m";
-    echo -e " \033[32m| .__/ \__, |\__|_| |_|\___/|_| |_|\033[34m\___|\033[35m\__,_|\033[36m\__,_|\033[36mhttps://github.com/pythoneda-shared\033[0m";
-    echo -e " \033[32m| |     __/ |                                       \033[36mhttps://github.com/pythoneda\033[0m";
-    echo -e " \033[32m|_|    |___/                                        \033[37mhttps://patreon.com/rydnr\033[0m";
+    echo -e " \033[32m| '_ \| | | | __| '_ \ / _ \| '_ \ \033[34m/ _ \\\\\033[35m/ _\` |\033[36m/ _\` |\033[34mhttps://github.com/pythoneda/base\033[0m";
+    echo -e " \033[32m| |_) | |_| | |_| | | | (_) | | | |\033[34m  __/\033[35m (_| |\033[36m (_| |\033[36mhttps://github.com/pythoneda\033[0m";
+    echo -e " \033[32m| .__/ \__, |\__|_| |_|\___/|_| |_|\033[34m\___|\033[35m\__,_|\033[36m\__,_|\033[37mhttps://patreon.com/rydnr\033[0m";
+    echo -e " \033[32m| |     __/ |\033[0m";
+    echo -e " \033[32m|_|    |___/\033[0m";
     echo;
     echo " Thank you for using PythonEDA, and for your appreciation of free software.";
     echo;
